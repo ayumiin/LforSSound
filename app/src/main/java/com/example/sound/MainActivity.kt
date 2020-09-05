54274071845bfcac9a689b8f306b37d9ae77f70a
ayumiin <rsamuv77@gmail.com>
2020-09-06 00:55:48 +0900

    サウンドアプリ2

--- app/src/main/java/com/example/sound/MainActivity.kt
+++ app/src/main/java/com/example/sound/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.sound
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -14,14 +15,35 @@ class MainActivity : AppCompatActivity() {
         //ドラムのサウンドファイルを読み込んで、プレイヤーをつくる
         val drumSound = MediaPlayer.create(this,R.raw.drum_sound)
 
-        //ドラムがタップされた時に呼ばれる
-        drumImage.setOnClickListener{
+       drumImage.setOnTouchListener { view, event ->
 
-            //ドラムの音を巻き戻す
-            drumSound.seekTo(0)
+           //タッチが始まったときに
+           if(event.action == MotionEvent.ACTION_DOWN){
 
-            //ドラムの音を再生する
-            drumSound.start()
-        }
+               //ドラムが鳴っている画像に切り替える
+               drumImage.setImageResource(R.drawable.drum_playing_image)
+
+               //ドラムの音を巻き戻す
+               drumSound.seekTo(0)
+
+               //ドラムの音を再生する
+               drumSound.start()
+           }
+
+           //タッチが終わるときに
+           else if(event.action == MotionEvent.ACTION_UP){
+
+               drumImage.setImageResource(R.drawable.drum_image)
+           }
+
+           else if(event.action == MotionEvent.ACTION_UP){
+
+               drumImage.setImageResource(R.drawable.drum_image)
+           }
+           true
+
+
+       }
+
     }
 }
